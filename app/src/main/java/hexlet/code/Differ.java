f22cf3fc52f112fd459eddc2f5c5433043bf85f3
ArturStimbiris <artur.stimbirys@gmail.com>
2024-11-13 21:02:59 +0300

    add file parser

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,15 @@
 package hexlet.code;
-
-import java.util.ArrayList;
+import java.util.Map;
 
 public class Differ {
     public static void generate(String filePath1, String filePath2) {
-        System.out.println("Hello, World!");
+        Map<String, Object> map1 = Parser.readJsonToMap(filePath1);
+        Map<String, Object> map2 = Parser.readJsonToMap(filePath2);
+
+        if (!map1.isEmpty() && !map2.isEmpty()) {
+            System.out.println("Парсинг JSON в Map:");
+            System.out.println(map1);
+            System.out.println(map2);
+        }
     }
 }
