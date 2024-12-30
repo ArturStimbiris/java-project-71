21034686ffd246e6806096f69600ed5af0b13899
ArturStimbiris <artur.stimbirys@gmail.com>
2024-12-30 18:03:20 +0300

    add new parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -41,4 +41,45 @@ public class Parser {
         }
         return map;
     }
+
+    public static Map<String, Object> parseYaml(String yaml) {
+        Map<String, Object> map = new HashMap<>();
+        yaml = yaml.trim();
+        String[] lines = yaml.split("\n");
+
+        for (String line : lines) {
+            line = line.trim();
+            if (line.isEmpty() || line.startsWith("#")) {
+                continue;
+            }
+
+            String[] keyValue = line.split(":");
+            String key = keyValue[0].trim();
+
+            Object value;
+            if (keyValue.length > 1) {
+                value = keyValue[1].trim();
+                if (value.startsWith("\"") && value.endsWith("\"")) {
+                    value = value.substring(1, value.length() - 1);
+                }
+            } else {
+                value = "";
+            }
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    public static Map<String, Object> readYamlToMap(String path) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            String yamlString = Files.readString(Paths.get(path));
+            map = parseYaml(yamlString);
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+        return map;
+    }
 }
